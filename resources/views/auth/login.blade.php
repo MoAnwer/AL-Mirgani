8a0e1925b777d743c5e98b09353d36ff1206621f
mohamed anwer <mhmdlyman362@gmail.com>
2025-09-21 05:47:22 +0200

    create header blade component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,4 @@
-<!doctype html>
-
-<html
-  lang="en" dir="rtl"
-  class="layout-wide customizer-hide"
-  data-assets-path="{{ asset('assets') }}">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-
-    <title>{{ $title ?? '' }}</title>
-
-    <meta name="description" content="" />
-
-    <!-- Favicon -->
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet" />
-
-    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/iconify-icons.css') }}" />
-
-    <!-- Core CSS -->
-    <!-- build:css assets/assets/vendor/css/theme.css  -->
-
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/demo.css') }}" />
-
-    <!-- assets/Vendors CSS -->
-
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
-    <!-- endbuild -->
-
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
-
-    <!-- Helpers -->
-    <script src="{{ asset('assets/vendor/js/helpers.js')}}"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-
-    <script src="{{ asset('assets/js/config.js')}}"></script>
-  </head>
-  <body>
+<x-header title="{{ $title }}" />
 
     <div class="container-xxl">
       <div class="authentication-wrapper authentication-basic container-p-y">
@@ -174,29 +123,3 @@
         </div>
       </div>
     </div>
-
-    <!-- Core JS -->
-
-    <script src="{{asset('assets/vendor/libs/jquery/jquery.js')}}"></script>
-
-    <script src="{{asset('assets/vendor/libs/popper/popper.js')}}"></script>
-    <script src="{{asset('assets/vendor/js/bootstrap.js')}}"></script>
-
-    <script src="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
-
-    <script src="{{asset('assets/vendor/js/menu.js')}}"></script>
-
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-
-    <script src="{{asset('assets/js/main.js')}}"></script>
-
-    <!-- Page JS -->
-
-    <!-- Place this tag before closing body tag for github widget button. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-  </body>
-</html>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html
+  lang="ar" dir="rtl"
+  class="layout-wide customizer-hide"
+  data-assets-path="{{ asset('assets') }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+     <title>{{ $title ?? '' }}</title>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/iconify-icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/demo.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
+    <script src="{{ asset('assets/vendor/js/helpers.js')}}"></script>
+    <script src="{{ asset('assets/js/config.js')}}"></script>
+  </head>
+  <body>
