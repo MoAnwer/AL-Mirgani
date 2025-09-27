501caf78b31c9d71a0d20c0bc66c3dee8c49021b
mohamed anwer <mhmdlyman362@gmail.com>
2025-09-27 15:01:44 +0200

    add messages in app.php lang/ar file

--- lang/ar/app.php
+++ lang/ar/app.php
@@ -3,11 +3,14 @@
 return [
     'app_name'              => 'مؤسسة الميرغني',
     'student'               => 'التلميذ',
+    'students'              => 'تلاميذ',
     'students_list'         => 'قائمة التلاميذ',
     'dashboard_title'       =>  'لوحة التحكم',
     'settings'              => 'الاعدادات',
     'student_not_found'     => 'لم يتم ايجاد التلميذ',
     'total_fee'             => 'الرسوم الدراسية',
+    'student_name'          => 'اسم التلميذ',
+    'student_number'        => 'الرقم المدرسي',
     'student_full_name'     => 'اسم التلميذ رباعي',
     'parent_full_name'      => 'اسم ولي الامر',
     'stage'                 => 'المرحلة',
@@ -19,5 +22,13 @@ return [
     'school'                => 'المدرسة',
     'edit'                  => 'تعديل',
     'delete'                => 'حذف',
-    'update_successful'     => 'تم تعديل بيانات :attribute بنجاح'
+    'actions'               => 'العمليات',
+
+
+    // Messages
+
+    'update_successful'         => 'تم تعديل بيانات :attribute بنجاح',
+    'delete_confirm_message'    => 'هل تريد حذف :attribute حقاً ؟',
+    'delete_successful'         => 'تم حذف بيانات :attribute بنجاح',
+    'empty_message'             => 'لا يوجد :attributes'
 ];
