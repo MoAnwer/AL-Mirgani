96c60a9de613e51458846e3e2de2ac5fa108bad9
mohamed anwer <mhmdlyman362@gmail.com>
2025-09-28 14:02:16 +0200

    create expense and expenses categories models and migrations

---
+++ app/Models/Expense.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\ReadableHumanDate;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    use ReadableHumanDate;
+    
+    protected $fillable = [
+        'amount',
+        'category_id', 
+        'school_id', 
+        'date', 
+        'statement', 
+        'user_id'
+    ];
+
+    public function category(): BelongsTo 
+    {
+        return $this->belongsTo(ExpenseCategory::class);
+    }
+
+    public function school(): BelongsTo
+    {
+        return $this->belongsTo(School::class);
+    }
+}
---
+++ app/Models/ExpenseCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\ReadableHumanDate;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ExpenseCategory extends Model
+{
+    use ReadableHumanDate;
+
+    protected $fillable = ['name'];
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+}
