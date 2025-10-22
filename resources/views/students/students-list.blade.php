35fec14dcc2c40a5ac0d09384adfeba3b8ab597b
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-22 20:13:38 +0200

    add search form in students list

--- resources/views/students/students-list.blade.php
+++ resources/views/students/students-list.blade.php
@@ -9,7 +9,14 @@
                         <x-alert type="error" />
                         <x-alert type="message" />
                         <div class="card"> 
-                            <h5 class="card-header">@lang('app.list', ['attribute' => __('app.the_students')])</h5>
+                            <div class="card-header d-flex align-items-center justify-content-between">
+                                <h4>@lang('app.list', ['attribute' => __('app.the_students')])</h4>
+                                <form action={{ URL::current() }}>
+                                    <div class="input-group">
+                                        <input type="search" placeholder="{{ __('app.search') }} ..." class="form-control" name="search" value="{{ request('search') }}" />
+                                    </div>
+                                </form>
+                            </div>
                             <x-Table.BasicTable>
                                 <x-Table.Thead>
                                     <td>#</td>
@@ -24,7 +31,8 @@
                                 <x-Table.Tbody>
                                     @forelse($students as $student)
                                         <tr>
-                                            <td>{{ ++$loop->index }}</td>
+                                        
+                                            <td>{{ ++$loop->index . '  ' . $student->count}}</td>
                                             <td>{{ $student->full_name }}</td>
                                             <td>{{ $student->student_number }}</td>
                                             <td>{{ $student->address }}</td>
@@ -48,7 +56,7 @@
                                         <td colspan="8" class="text-center"> {{ __('app.empty_message', ['attributes' => __('app.students')]) }} </td>
                                     @endforelse
                                     @section('pagination')
-                                        <div class="mt-5 px-5">{{ $students->links('vendor.pagination.bootstrap-5') }} </div>
+                                        <div class="mt-5 px-5">{{ $students->withQueryString()->links('vendor.pagination.bootstrap-5') }} </div>
                                     @endsection
                                 </x-Table.Tbody>
                             </x-Table.BasicTable>
