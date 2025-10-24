8a99bd6f052c5ede7fef62d4a665e105fcab3348
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-24 21:01:48 +0200

    remove unused revenues cards

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -15,8 +15,7 @@
                     <h3 class="mb-5">التقارير المالية 💰</h3>
                     <x-cards.cards-container>
                         <x-cards.card link="{{ route('arrears.all') }}" icon="bx bx-money" title=" الأقساط المتأخرة" description="نظرة عامة عن متاخرات الاقساط"/>
-                        <x-cards.card link="{{ route('revenues.schools', 1) }}" icon='bx bxs-bar-chart-alt-2' title="ايرادات حسب الصف للمدرسة الابتدائية" description="ايرادات حسب الصف للمدرسة الابتدائية"/>
-                        <x-cards.card link="{{ route('revenues.schools', 2) }}" icon='bx bxs-bar-chart-alt-2' title="ايرادات حسب الصف للمدرسة المتوسطة" description="ايرادات حسب الصف للمدرسة المتوسطة"/>
+                        <x-cards.card link="{{ route('revenues') }}" icon='bx bxs-bar-chart-alt-2' title="ايرادات حسب الصف" description="ايرادات حسب الصف للمدرسة الابتدائية"/>
                         <x-cards.card link="{{ route('incomeReport') }}" icon='bx bxs-bar-chart-alt-2' title="قائمة الدخل" description="قائمة الدخل"/>
                     </x-cards.cards-container>
                 </div>
