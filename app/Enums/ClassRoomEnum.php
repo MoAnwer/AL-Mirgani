61d6390c375bb87efdca8a9a7bfa32666bab2e6e
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-25 11:23:52 +0200

    create class room enum and save classes with run class_room_table migration file

---
+++ app/Enums/ClassRoomEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum ClassRoomEnum: string
+{
+    case BASIC_ONE = "الاول الإبتدائي";
+    case BASIC_TWO = "الثاني الإبتدائي";
+    case BASIC_THREE = "الثالث الإبتدائي";
+    case BASIC_FOUR = "الرابع الإبتدائي";
+    case BASIC_FIVE = "الخامس الإبتدائي";
+    case BASIC_SIX = "السادس الإبتدائي";
+    case MIDDLE_ONE = "الاول المتوسط";
+    case MIDDLE_TWO = "الثاني المتوسط";
+    case MIDDLE_THREE = "الثالث المتوسط";
+}
