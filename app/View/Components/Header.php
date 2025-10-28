1f68dd0fce0b7c252787d5be3c2cb512ba27b156
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-28 17:12:50 +0200

    make description attribute optional

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -12,7 +12,7 @@ class Header extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $title
+        public string $title = 'Margane'
     ) {}
 
     /**
