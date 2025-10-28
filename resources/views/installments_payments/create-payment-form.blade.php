6a90cc43ed0c25f27d2c2636c3e23a73c7f6dc54
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-28 18:27:23 +0200

    replace notes field by set statement field name

--- resources/views/installments_payments/create-payment-form.blade.php
+++ resources/views/installments_payments/create-payment-form.blade.php
@@ -52,7 +52,7 @@
                                         <div class="col-md-12">
                                             <label class="mb-3 mt-4">@lang('app.notes')</label>
                                             <div class="input-group">
-                                              <textarea rows="5" class="form-control" name="notes">{{ old('notes') }}</textarea>
+                                              <textarea rows="5" class="form-control" name="statement">{{ old('statement') }}</textarea>
                                             </div>
                                         </div>
                                         <input type="hidden" name="student_id" value="{{ $installment->student->id }}"/>
