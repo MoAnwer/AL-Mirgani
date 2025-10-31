856c97af10ba5d4888fcc334e6f00ff7c9082267
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-31 16:46:51 +0200

    replace accounts card icon

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -14,7 +14,7 @@
                 <div class="dashboard-container">
                     <h3 class="mb-5">التقارير المالية 💰</h3>
                     <x-cards.cards-container>
-                        <x-cards.card link="{{ route('accounts') }}" icon="bx bx-money" title=" كشف الحساب " description=""/>
+                        <x-cards.card link="{{ route('accounts') }}" icon="bx bx-book-open" title=" كشف الحساب " description=""/>
                         <x-cards.card link="{{ route('arrears.all') }}" icon="bx bx-money" title=" الأقساط المتأخرة" description="نظرة عامة عن متاخرات الاقساط"/>
                         <x-cards.card link="{{ route('revenues') }}" icon='bx bxs-bar-chart-alt-2' title="ايرادات حسب الصف" description="ايرادات حسب الصف للمدرسة الابتدائية"/>
                         <x-cards.card link="{{ route('incomeReport') }}" icon='bx bxs-bar-chart-alt-2' title="قائمة الدخل" description="قائمة الدخل"/>
