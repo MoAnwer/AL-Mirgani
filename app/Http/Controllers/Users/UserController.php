1ba3dddf28a9a75ffff8f6e0dbd00761199337bd
mohamed anwer <mhmdlyman362@gmail.com>
2025-10-31 18:07:04 +0200

    show users list action

---
+++ app/Http/Controllers/Users/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    function __construct(
+        private User $user
+    ) {}
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = $this->user->latest()->paginate(15);
+
+        return view('users.users_list', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
