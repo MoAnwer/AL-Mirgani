267c870f70b3b719d556d430d9a7c4db94dbb2bf
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-05 09:18:53 +0200

    add expense report link  in reports page

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -18,6 +18,7 @@
                         <x-cards.card link="{{ route('payroll.summary.report') }}" icon="bx bx-box" title="  تقرير كشوف الرواتب " description="الإجماليات المُعالجة حسب الشهر و العام"/>
                         <x-cards.card link="{{ route('arrears.all') }}" icon="bx bx-money" title=" الأقساط المتأخرة" description="نظرة عامة عن متاخرات الاقساط"/>
                         <x-cards.card link="{{ route('revenues') }}" icon='bx bxs-bar-chart-alt-2' title="ايرادات حسب الصف" description="تحليل الإيرادات حسب الصف للمرحلة كل المدارس"/>
+                        <x-cards.card link="{{ route('reports.general-expense-report') }}" icon='bx bx-wallet' title="إجمالي المصروفات العامة للمدرسة" description="الإجمالي الكلي للمصروفات التشغيلية والمدرسية ."/>
                         <x-cards.card link="{{ route('incomeReport') }}" icon='bx bxs-bar-chart-alt-2' title="قائمة الدخل" description=" الإيرادات و المصروفات التشغيلية و صافي الربح"/>
                     </x-cards.cards-container>
                 </div>
