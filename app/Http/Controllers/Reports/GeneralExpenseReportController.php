b2902f872b42d6a1a41cb798280872de0b3533c0
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-05 19:49:59 +0200

    add expense category filter in general expense report

--- app/Http/Controllers/Reports/GeneralExpenseReportController.php
+++ app/Http/Controllers/Reports/GeneralExpenseReportController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Reports;
 
-use App\Http\Controllers\Controller;
-use App\Models\Expense;
-use App\Models\School;
 use Carbon\Carbon;
+use App\Enums\ExpenseCategoryEnum;
+use App\Http\Controllers\Controller;
+use App\Models\{Expense, ExpenseCategory, School};
 use Illuminate\Http\Request;
 
 class GeneralExpenseReportController extends Controller
 {
 
-    function __construct(private Expense $expense, private School $school) {}
+    function __construct(private Expense $expense, private School $school, private ExpenseCategory $expense_category) {}
 
 
    /**
@@ -22,15 +22,21 @@ class GeneralExpenseReportController extends Controller
      */
     public function generateGeneralExpenseSummary(Request $request)
     {
-        $schoolId = $request->input('school_id');
-        $startDate = $request->input('start_date');
-        $endDate = $request->input('end_date');     
+        $schoolId   = $request->input('school_id');
+        $startDate  = $request->input('start_date');
+        $endDate    = $request->input('end_date');     
+        $category   = $request->input('category_id');  
 
         $query = $this->expense
                         ->query()
                         ->when($schoolId,  fn($q, $school_id)     => $q->where('school_id', $school_id))
                         ->when($startDate, fn($q, $startDate)     => $q->whereDate('date', '>=', Carbon::parse($startDate)->toDateString()))
-                        ->when($endDate,   fn($q, $endDate)       => $q->whereDate('date', '<=', Carbon::parse($endDate)->toDateString()));
+                        ->when($endDate,   fn($q, $endDate)       => $q->whereDate('date', '<=', Carbon::parse($endDate)->toDateString()))
+                        ->when($category,  
+                            function($q) use ($category) {
+                                $q->whereHas('category', fn($q)   => $q->where('category_id', $category));
+                            }
+                        );
 
         
         $totalExpenses = $query->sum('amount');
@@ -41,6 +47,8 @@ class GeneralExpenseReportController extends Controller
 
         $schools = $this->school->get();
 
-        return view('reports.general_expense_summary', compact('reportData', 'schoolId', 'startDate', 'endDate', 'schools'));
+        $categories = $this->expense_category->pluck('id', 'name');
+
+        return view('reports.general_expense_summary', compact('reportData', 'schoolId', 'startDate', 'endDate', 'schools', 'categories'));
     }
 }
