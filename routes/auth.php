87b3a38685dd5b0fb19e1a4bc1b8c0b484d77ffb
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-08 17:13:29 +0200

    create auth routes file

---
+++ routes/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+
+Route::name('auth.')->group(function() {
+
+    Route::name('login.')->middleware('guest')->group(function() {
+        Route::get('login', [LoginController::class, 'login'])->name('form');
+        Route::post('loginAction', [LoginController::class, 'loginAction'])->name('action');
+    });
+    
+    Route::post('logout', LogoutController::class)->name('logout');
+});
