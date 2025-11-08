b7a2e51041b77dbeb916fee326d52e4efe6fa706
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-08 13:07:39 +0200

    create payrolls route file

---
+++ routes/payrolls.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Employees\EmployeePayrollController;
+use App\Http\Controllers\Reports\PayrollSummaryReportController;
+
+Route::name('payroll.')
+    ->prefix('payrolls')
+    ->controller(EmployeePayrollController::class)
+    ->group(function () {
+        Route::get('', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/{payroll}', 'show')->name('show');
+        Route::get('/{payroll}/edit', 'edit')->name('edit');
+        Route::get('/{payroll}/delete', 'delete')->name('delete');
+        Route::delete('/{payroll}/destroy', 'destroy')->name('destroy');
+        Route::put('/{payroll}/update', 'update')->name('update');
+        Route::get('/{payroll}/print', 'payrollInvoice')->name('invoice.print');
+    });
+
+
+Route::get('/payroll-summary-report', [PayrollSummaryReportController::class, 'generateSummaryReport'])->name('payroll.summary.report');
