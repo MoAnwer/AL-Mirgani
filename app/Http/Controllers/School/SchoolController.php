2d2f2391fa2bd12f3158e4c53c92434bc33ebbc0
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-09 10:02:28 +0200

    finish schools list, create page, edit page

---
+++ app/Http/Controllers/School/SchoolController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\School;
+
+use App\Http\Controllers\Controller;
+use App\Models\School;
+use Illuminate\Http\Request;
+
+class SchoolController extends Controller
+{
+
+    public function __construct(private readonly School $school) {}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $schools = $this->school->latest()->paginate(15);
+
+        return view('schools.schools-list', compact('schools'));
+    }
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('schools.create-school');
+    }
+
+
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $data = $request->validate(['name' => 'required|string']);
+
+            $this->school->create($data);
+
+            return to_route('schools.index')->with('message', __('app.create_successful', ['attribute' => __('app.school')]));
+
+        } catch (\Throwable $th) {
+
+            report($th);
+
+            return back()->with('error', __('app.error'));
+        }
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(School $school)
+    {
+        return view('schools.edit-school', compact('school'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, School $school)
+    {
+        try {
+
+            $data = $request->validate(['name' => 'required|string']);
+
+            $school->update($data);
+
+            return to_route('schools.index')->with('message', __('app.update_successful', ['attribute' => __('app.school')]));
+
+        } catch (\Throwable $th) {
+
+            report($th);
+
+            return back()->with('error', __('app.error') . ' ' . $th->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(School $school)
+    {
+        // TODO: delete school
+    }
+}
---
+++ resources/views/schools/create-school.blade.php
@@ -0,0 +1,51 @@
+<x-header title="اضافة مدرسة جديدة" />
+<x-LayoutWrapper>
+    <x-LayoutContainer>
+        <x-aside />
+        <x-LayoutPage>
+            <x-nav />
+            <x-ContentWrapper>
+                <x-Container>
+                    <div class="row row g-6 mb-6">
+                        <div class="col-md-12">
+                            <div class="card container">
+
+                                <x-alert type="message" />
+                                <x-alert type="error" />
+                            
+                                <h4 class="card-header">اضافة مدرسة جديدة</h4>
+
+                                @if($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
+                                <div class="card-body">
+                                
+                                    <form action="{{ route('schools.store') }}" method="POST">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label class="form-label">@lang('app.school')</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="name" value="{{ old('name') }}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class=" mt-4 btn btn-primary">اضافة</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </x-Container>
+            </x-ContentWrapper>
+        </x-LayoutPage>
+    </x-LayoutContainer>
+</x-LayoutWrapper>
+<x-footer/> 
