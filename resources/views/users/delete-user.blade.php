e7da72e46427ce6a049b8a070063bec7b57a1102
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-10 18:13:38 +0200

    create delete user page

--- resources/views/users/delete-user.blade.php
+++ resources/views/users/delete-user.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<x-header title="{{ __('app.delete') .' '. __('app.user') . ' '  . $user->name}}" />
+<x-container>
+    <div class="container">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authenticatin-inner">
+                <div class="card px-sm-6 px-0">
+                    <div class="card-body">
+                        <form class="mb-6" action="{{ route('users.destroy', $user) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <h3 class="m-5 text-center">@lang('app.delete_confirm_message', ['attribute' => __('app.user').' '.$user->name])</h3>
+                            <button class="mt-10 btn btn-danger w-100" type="submit"><i class="icon-base bx bx-trash"></i> @lang('app.delete')</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</x-container>
