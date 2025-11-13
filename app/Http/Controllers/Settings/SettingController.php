c2b0ebbde89daa2a43997ac8c99742b1fca43fc3
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-13 15:08:22 +0200

    provide security questions data in settings page

--- app/Http/Controllers/Settings/SettingController.php
+++ app/Http/Controllers/Settings/SettingController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
+use App\Models\SecurityQuestion;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    public function settingsPage() {
-        return view('settings.settings-page');
+    public function __construct(private readonly SecurityQuestion $securityQuestion) {}
+
+
+    public function settingsPage()
+    {
+        $securityQuestions = $this->securityQuestion->where('user_id', auth()->user()->id)->get();
+
+        return view('settings.settings-page', compact('securityQuestions'));
     }
 }
