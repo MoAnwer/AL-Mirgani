8db50039e212b5d2db74ed2afef1d3c1d9451db7
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-13 16:42:40 +0200

    define the edit,update and delete security question routes

--- routes/settings.php
+++ routes/settings.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SecurityQuestion\SecurityQuestionController;
 use App\Http\Controllers\Settings\SettingController;
 
 Route::name('settings.')->prefix('settings')->group(function () {
     Route::get('', [SettingController::class, 'settingsPage'])->name('page');
+    Route::get('/create/security-question', [SecurityQuestionController::class, 'create'])->name('create_security_question');
+    Route::post('/store/security-question', [SecurityQuestionController::class, 'store'])->name('store_security_question');
+    Route::get('/security-question/edit/{securityQuestion}', [SecurityQuestionController::class, 'edit'])->name('edit_security_question');
+    Route::put('/security-question/update/{securityQuestion}', [SecurityQuestionController::class, 'update'])->name('update_security_question');
+    Route::delete('/security-question/delete/{securityQuestion}', [SecurityQuestionController::class, 'destroy'])->name('delete_security_question');
 });
