d450e286081a2690bcc33783b233910af94d4388
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-13 10:56:26 +0200

    define forgot password routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 
@@ -8,6 +9,10 @@ Route::name('auth.')->group(function() {
     Route::name('login.')->middleware('guest')->group(function() {
         Route::get('login', [LoginController::class, 'login'])->name('form');
         Route::post('loginAction', [LoginController::class, 'loginAction'])->name('action');
+    });
+
+    Route::name('forgot_password.')->prefix('forgot-password')->middleware('guest')->group(function() {
+        Route::get('', [ForgotPasswordController::class, 'forgotPasswordPage'])->name('page');
     });
     
     Route::post('logout', LogoutController::class)->name('logout');
