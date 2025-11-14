aaaad3df957934aa8da9e9935f44569ff1b89b89
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-14 14:23:07 +0200

    create english notification localization file

---
+++ lang/en/notifications.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'titles'    => [
+        'payroll_paid'      => 'Payroll paid',
+        'installment_paid'  => 'Installment paid',
+        'password_reset'    => 'Security: Password Reset',
+        'new_user_added'    => 'Added new user',
+        'user_deleted'      => 'Deleted user',
+        'employee_added'    => 'Add new employee',
+        'employee_deleted'  => 'Deleted employee',
+        'new_school_added'  => 'Add new school',
+        'school_deleted'    => 'Deleted school',
+    ],
+    'messages' => [
+        'payroll_paid'      => "Paid payroll of :employee for month :month/:year",
+        'installment_paid'  => 'Installment :number of :student is fully paid',
+        'password_reset'    => ':user, your password reset successfully',
+        'new_user_added'    => 'New user :user added to system successfully',
+        'user_deleted'      => 'User :user deleted from system successfully',
+        'employee_added'    => 'New employee :employee added to system successfully',
+        'employee_deleted'  => 'Employee :employee deleted from system successfully',
+        'new_school_added'  => 'New school :school added to system successfully',
+        'school_deleted'    => 'School :school deleted from system successfully',
+    ]
+];
