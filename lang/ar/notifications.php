0e0c27c42a923c26417abe0a796eda74edfedb59
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-14 14:22:33 +0200

    create arabic notification localization file

---
+++ lang/ar/notifications.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'titles'    => [
+        'payroll_paid'      => 'دفع مرتب موظف',
+        'installment_paid'  => 'سداد قسط',
+        'password_reset'    => 'امان : اعادة تعيين كلمة السر',
+        'new_user_added'    => 'اضافة مستخدم جديد',
+        'user_deleted'      => 'تم حذف مستخدم',
+        'employee_added'    => 'اضافة موظف جديد',
+        'employee_deleted'  => 'حذف مستخدم',
+        'new_school_added'  => 'اضافة مدرسة جديدة',
+        'school_deleted'    => 'حذف مدرسة',
+    ],
+    'messages' => [
+        'payroll_paid'      => "تم دفع مرتب الموظف :employee  لشهر  :month/:year",
+        'installment_paid'  => 'تم اكمال دفع القسط رقم :number الخاص بالطالب :student',
+        'password_reset'    => ':user, تم اعادة تعيين كلمة السر الخاصة بك',
+        'new_user_added'    => 'تم اضافة المستخدم :user بنجاح الى النظام',
+        'user_deleted'      => 'تم حذف المستخدم :user من النظام بنجاح',
+        'employee_added'    => 'تم اضافة الموظف :employee بنجاح الى النظام',
+        'employee_deleted'  => 'تم حذف الموظف :employee من النظام بنجاح',
+        'new_school_added'  => 'تم اضافة المدرسة :school بنجاح الى النظام',
+        'school_deleted'    => 'تم حذف المدرسة :school من النظام بنجاح',
+    ]
+];
