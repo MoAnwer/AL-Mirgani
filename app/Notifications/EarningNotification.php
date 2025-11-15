d2f4bc3af1cb4278d0e143f032f639c8ca4e6529
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-15 14:52:31 +0200

    create the EarningNotification

---
+++ app/Notifications/EarningNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Earning;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class EarningNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private readonly Earning $earning) {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'icon'        => 'bx bx-money',
+            'color'       => 'success',
+            'title'       => __('notifications.titles.new_earning'),
+            'message'     => __('notifications.messages.new_earning', [
+                'amount'  => $this->earning->amount,
+                'school'  => $this->earning->school->name
+            ])
+        ];
+    }
+}
