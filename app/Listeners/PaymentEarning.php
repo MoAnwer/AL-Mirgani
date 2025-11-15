0eb9bda1921f8f6acddd6274712c039dcb4cd2e7
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-15 14:35:45 +0200

    make the PaymentEarning send to all users

--- app/Listeners/PaymentEarning.php
+++ app/Listeners/PaymentEarning.php
@@ -4,9 +4,9 @@ namespace App\Listeners;
 
 use App\Events\Earning\InstallmentPaymentIsPaid;
 use App\Models\Earning;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use App\Models\User;
+use App\Notifications\EarningNotification;
+use Illuminate\Support\Facades\Notification;
 
 class PaymentEarning
 {
@@ -20,11 +20,13 @@ class PaymentEarning
      */
     public function handle(InstallmentPaymentIsPaid $event): void
     {
-        $this->earning->create([
+        $earning = $this->earning->create([
             'amount'    => $event->payment->paid_amount,
             'school_id' => $event->payment->student->school->id,
             'statement' => $event->payment->statement,
             'date'      => now()
         ]);
+
+        Notification::send(User::all(), new EarningNotification($earning));
     }
 }
