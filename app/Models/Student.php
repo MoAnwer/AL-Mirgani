c95b0fd44709e1b65c6c7f730a6c7f574329b5bd
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-21 19:37:35 +0200

    add check condition in totalPaid methods in student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -78,7 +78,10 @@ class Student extends Model
 
     public function totalPaidBetween($startDate, $endDate) {
         return $this->payments->whereBetween('payment_date', [$startDate, $endDate])?->sum(function($payment) {
-            return $payment?->paid_amount ?? 0;
+            // Check if the payment has receipt
+            if ($payment->receipt_number != null) {
+                return $payment?->paid_amount ?? 0;
+            }
         });
     }
 }
