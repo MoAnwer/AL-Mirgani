180d7a5001093db0a4023535a3c0d0646a5ff31d
mohamed anwer <mhmdlyman362@gmail.com>
2025-11-27 09:05:41 +0200

    Verify notifications database file exists before running the notification migration file

--- database/migrations/2030_11_14_082658_create_notifications_table.php
+++ database/migrations/2030_11_14_082658_create_notifications_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -12,15 +13,17 @@ return new class extends Migration
     public function up(): void
     {
         if (!file_exists(database_path('notifications.sqlite'))) {
-            Schema::connection('notifications')->create('notifications', function (Blueprint $table) {
-                $table->uuid('id')->primary();
-                $table->string('type');
-                $table->morphs('notifiable');
-                $table->text('data');
-                $table->timestamp('read_at')->nullable();
-                $table->timestamps();
-            });
+            touch(database_path('notifications.sqlite'));
         }
+
+        Schema::connection('notifications')->create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
