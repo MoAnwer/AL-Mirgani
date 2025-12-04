2b880858d7d8909ae88f4c58d61f7a7ecd4f7e0e
mohamed anwer <mhmdlyman362@gmail.com>
2025-12-04 07:03:44 +0200

    Add  form-password-toggle in create user form

--- resources/views/users/create-user.blade.php
+++ resources/views/users/create-user.blade.php
@@ -9,55 +9,55 @@
                 <x-Container>
                     <div class="card -sm border-0 mb-5">
                         <div class="card-body p-4 p-md-5">
-                                <x-alert type="message" />
-                                <x-alert type="error" />
+                            <x-alert type="message" />
+                            <x-alert type="error" />
 
-                                @if($errors->any())
-                                    <div class="alert alert-danger">
-                                        <ul>
-                                        @foreach($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                        </ul>
-                                    </div>
-                                @endif
-                                <div class="p-3  border -sm rounded-3 mb-5">
-                                    <div class="card-header border-bottom pt-1 pb-3 mb-5">
-                                        <h4>{{ __('app.create', ['attribute' => __('app.user')]) }}</h4>
-                                    </div>
+                            @if($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <div class="p-3  border -sm rounded-3 mb-5">
+                                <div class="card-header border-bottom pt-1 pb-3 mb-5">
+                                    <h4>{{ __('app.create', ['attribute' => __('app.user')]) }}</h4>
+                                </div>
 
-                                    <form action="{{ route('users.store') }}" method="POST">
-                                        @csrf
-                                        <div class="row">
-                                            <div class="col-md-4 col-sm-6">
-                                                <label class="form-label mb-2">@lang('app.name')</label>
-                                                <div class="input-group">
-                                                    <input type="text" class="form-control" name="name" value="{{ old('name') }}" />
-                                                </div>
+                                <form action="{{ route('users.store') }}" method="POST">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-4 col-sm-6">
+                                            <label class="form-label mb-2">@lang('app.name')</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="name" value="{{ old('name') }}" />
                                             </div>
-                                            <div class="col-md-4 col-sm-6">
-                                                <label class="form-label mb-2">@lang('app.username')</label>
-                                                <div class="input-group">
-                                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" />
-                                                </div>
+                                        </div>
+                                        <div class="col-md-4 col-sm-6">
+                                            <label class="form-label mb-2">@lang('app.username')</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="username" value="{{ old('username') }}" />
                                             </div>
-                                            <div class="col-md-4 col-sm-6">
-                                                <label class="form-label mb-2">@lang('app.password')</label>
-                                                <div class="input-group">
-                                                    <input type="password" class="form-control" name="password" value="{{ old('password') }}" />
-                                                </div>
+                                        </div>
+                                        <div class="col-md-4 mb-6 form-password-toggle">
+                                            <label class="form-label" for="password"> {{ __('app.password') }} </label>
+                                            <div class="input-group input-group-merge">
+                                                <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                                                <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
                                             </div>
                                         </div>
                                         <button type="submit" class="mt-7 btn btn-primary">@lang('app.save')</button>
-                                    </form>
+                                </form>
 
-                                </div>
                             </div>
                         </div>
+                    </div>
                 </x-Container>
             </x-ContentWrapper>
         </x-LayoutPage>
     </x-LayoutContainer>
 </x-LayoutWrapper>
 
-<x-footer/> 
+<x-footer />
