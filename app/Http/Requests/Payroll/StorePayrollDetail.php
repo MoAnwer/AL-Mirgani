be0b4ab79e845d2f42fe2c5bfee4e7a0578ff193
mohamed anwer <mhmdlyman362@gmail.com>
2025-12-06 14:34:43 +0200

    Create store payroll detail request

---
+++ app/Http/Requests/Payroll/StorePayrollDetail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Payroll;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class StorePayrollDetail extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'item_id' => [
+                'required',
+                'exists:payroll_items,id',
+                // Ensure this item hasn't already been added to this specific payroll
+                Rule::unique('payroll_details')->where(function ($query) {
+                    return $query->where('payroll_id', $this->payroll->id);
+                }),
+            ],
+            'amount' => 'required|numeric|min:0.01|max_digits:15',
+            'notes' => 'nullable|string|max:255',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'item_id' => __('app.item')
+        ];
+    }
+}
