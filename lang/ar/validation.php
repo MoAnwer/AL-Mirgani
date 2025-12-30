3222217ca3c27cef712512167e1c9213ec640aa5
mohamed anwer <mhmdlyman362@gmail.com>
2025-12-30 20:32:32 +0200

    Add new validations in ar/validation.php file

--- lang/ar/validation.php
+++ lang/ar/validation.php
@@ -2,6 +2,7 @@
 
 return [
     'required'  => 'حقل :attribute مطلوب',
+    'required_if' => 'حقل :attribute مطلوب عندما يكون حقل :other قيمته :value',
     'in'        => 'الرجاء اختيار :attribute من الخيارات الموجودة',
     'min' => ['numeric' => ' :attribute يجب ان تكون من :min احرف على الاقل'],
     'date'      => 'الرجاء ادخال تاريخ صالح',
@@ -11,9 +12,10 @@ return [
     'max_digits' => 'حقل :attribute يجب ان لا يزيد عن اكثر من :max خانات',
     'max' => [
         'string' => 'حقل :attribute يجب ان لا يتجاوز طوله اكثر من :max حرف',
+        'numeric' => 'حقل :attribute يجب ان لا يكون اكبر من :max.'
     ],
     'password' => [
         'numbers' => 'يجب ان تحتوي كلمة السر على ارقام',
         'symbols' => 'يجب ان تحتوي كلنة السر على رموز خاصة (مثل #@)',
-    ]
+    ],
 ];
