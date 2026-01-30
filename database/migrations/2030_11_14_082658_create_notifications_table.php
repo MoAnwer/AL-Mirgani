35f0121b401dd2fadf24e8473c1f49cb00e03267
mohamed anwer <mhmdlyman362@gmail.com>
2026-01-30 12:56:04 +0200

    Fix: add check if notification table is exists

--- database/migrations/2030_11_14_082658_create_notifications_table.php
+++ database/migrations/2030_11_14_082658_create_notifications_table.php
@@ -13,17 +13,21 @@ return new class extends Migration
     public function up(): void
     {
         if (!file_exists(database_path('notifications.sqlite'))) {
+            // Create new sqlite database file
             touch(database_path('notifications.sqlite'));
         }
 
-        Schema::connection('notifications')->create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('type');
-            $table->morphs('notifiable');
-            $table->text('data');
-            $table->timestamp('read_at')->nullable();
-            $table->timestamps();
-        });
+        // Create notifications table if not exists
+        if (! Schema::connection('notifications')->hasTable('notifications')) {
+            Schema::connection('notifications')->create('notifications', function (Blueprint $table) {
+                $table->uuid('id')->primary();
+                $table->string('type');
+                $table->morphs('notifiable');
+                $table->text('data');
+                $table->timestamp('read_at')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
