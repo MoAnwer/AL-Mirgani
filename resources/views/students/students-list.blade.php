d801ddbab5a62b1c3c3c56f1ecc8516f714ed980
mohamed anwer <mhmdlyman362@gmail.com>
2026-02-01 11:24:14 +0200

    Fix: check student stage and class not null to display them

--- resources/views/students/students-list.blade.php
+++ resources/views/students/students-list.blade.php
@@ -44,10 +44,10 @@
                                     <td>{{ $student->id }}</td>
                                     <td>{{ $student->full_name }}</td>
                                     <td>{{ $student->student_number }}</td>
-                                    <td>{{ $student->address }}</td>
-                                    <td>{{ is_null($student->class->name) ? __("classes.{$student->class->name}") : __('app.not_specify') }}</td>
-                                    <td>{{ !is_null($student->stage) ? __("classes.{$student->stage}") : __('app.not_specify')  }}</td>
-                                    <td>{{ $student->school->name }}</td>
+                                    <td>{{ $student->address != null ? $student->address : __('app.not_specified') }}</td>
+                                    <td>{{ __("classes.{$student->class->name}") }}</td>
+                                    <td>{{ $student->stage != null ? __("classes.{$student->stage}") : __('app.not_specified')  }}</td>
+                                    <td>{{  $student->school->name  }}</td>
                                     <td>
                                         <div class="dropdown">
                                             <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
