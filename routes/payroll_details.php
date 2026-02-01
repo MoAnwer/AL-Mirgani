85ee3ecbd4aeee180466fb96844230e0bbd99165
mohamed anwer <mhmdlyman362@gmail.com>
2026-02-01 10:55:11 +0200

    add delete and destory routes in payroll_details route file

--- routes/payroll_details.php
+++ routes/payroll_details.php
@@ -2,10 +2,12 @@
 use App\Http\Controllers\Payrolls\PayrollDetailController;
 use App\Http\Middleware\EmployeeIsExists;
 
+Route::get('payroll-details/{detail}/delete', [PayrollDetailController::class, 'delete'])->name('payroll.details.delete');
+Route::delete('payroll-details/{detail}/destroy', [PayrollDetailController::class, 'destroy'])->name('payroll.details.destroy');
+
 Route::middleware(EmployeeIsExists::class)->prefix('payroll/{payroll}/details')->name('payroll.details.')->group(function () {
     Route::get('/create', [PayrollDetailController::class, 'create'])->name('create');
     Route::post('/', [PayrollDetailController::class, 'store'])->name('store');
-    
     Route::get('/{detail}/edit', [PayrollDetailController::class, 'edit'])->name('edit');
     Route::put('/{detail}', [PayrollDetailController::class, 'update'])->name('update');
 });
